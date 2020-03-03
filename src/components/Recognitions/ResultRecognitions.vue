8159bd1a4787df496973da419dad81c6868af505
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-03-03 22:01:09 +0300

    add new functions

--- src/components/Recognitions/ResultRecognitions.vue
+++ src/components/Recognitions/ResultRecognitions.vue
@@ -1,73 +1,32 @@
 <template>
     <div>
-        <div v-for='(item, index) in resultObjArr' :key='index' 
+        <div v-for='(item, index) in getRecogResult.faceRecogResult' :key='index' 
             :class='{ colorBackOk : item.isRecog, colorBackNo : !item.isRecog }'
-            class='flexContainerResRec radius fullSize margVert border ordinarContainerAdp' >
-                <img class='flexItemAdp imgOfRec radius pointer'
-                    :src='require(`../../content/images/${item.recImg}`)' 
+            class='gridContainerResRecAdp radius margDown border simplePad' >
+                <img class='imgOfRecAdp fullSize radius pointer'
+                    :src='item.recImg' 
                 />
-                <span class='flexItemAdp titleOfRec simplePad upper' > 
-                    Person {{index+1}} <br/> {{getPersonDesc(item.isRecog)}} 
+                <span class='titleOfRecAdp upper' > 
+                    Person {{index+1}} {{getPersonDesc(item.isRecog)}} 
                 </span>
-                <div class='flexItemAdp' v-if='item.isRecog' >
-                    <img v-for='(cItem, cIndex) in item.docImgArr' :key='cIndex' 
-                        class='imgOfDoc minMarg radius pointer'
-                        :src='require(`../../content/images/${cItem}`)' />
-                </div>
+                <span v-for='(cItem, cIndex) in item.docImgArr' :key='cIndex' >
+                    <img v-if='item.isRecog'
+                        class='imgOfDocAdp radius pointer'
+                        :src='cItem' 
+                    />
+                </span>
         </div>
     </div>
 </template>
 <script>
+    import { mapGetters } from 'vuex'
     export default {
         data :()=>({
-            resultObjArr : [],
         }),
-        created(){
-            this.resultObjArr = [
-                {
-                    recImg : '0.jpg',
-                    docImgArr : [
-                        'doc_0.jpg'
-                    ],
-                    isRecog : true,
-                },
-                {
-                    recImg : '1.jpg',
-                    docImgArr : [
-                        'doc_1.jpg', 'doc_1_1.jpg', 'doc_1_1_1.jpg',
-                    ],
-                    isRecog : true,
-                },
-                {
-                    recImg : '2.jpg',
-                    docImgArr : [
-                        'doc_2.jpg', 'doc_2_2.jpg', 'doc_2_2_2.jpg',
-                    ],
-                    isRecog : false,
-                },
-                {
-                    recImg : '3.jpg',
-                    docImgArr : [
-                        'doc_3.jpg', 'doc_3_3.jpg'
-                    ],
-                    isRecog : true,
-                },
-                {
-                    recImg : '4.jpg',
-                    docImgArr : [ 
-
-                    ],
-                    isRecog : false,
-                },
-                {
-                    recImg : '5.jpg',
-                    docImgArr : [
-                        'doc_5.jpg', 'doc_5_5.jpg'
-                    ],
-                    isRecog : true,
-                },
-            ];
-            //this.resultObjArr = this.resultObjArr.filter( item=> item.recImg === '5.jpg' );
+        computed : {
+            ...mapGetters(['getRecogResult'])
+        },
+        created() {
         },
         methods : {
             getPersonDesc(isRecog){
@@ -83,20 +42,7 @@
     .colorBackNo {
         background-color : #F8CBAD;
     }
-    .flexContainerResRec {
-        display : flex;
-        flex-flow : row wrap;
-        justify-content : space-between;
-    }
-    .titleOfRec {
-        line-height : 45px; 
-        width : 55%;
-    }
-    .imgOfRec {
-        width : 45%    
-    }
-    .imgOfDoc {
-        width  : 20vw;
-        height : 10vh;
+    .rezOfRecAdp img {
+        float : left;
     }
 </style>
