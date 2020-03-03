99cb105864a3537af78d677b5bf2a02888fa3c02
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-03-03 22:00:44 +0300

    add new functions

--- src/components/Recognitions/FaceRecognition.vue
+++ src/components/Recognitions/FaceRecognition.vue
@@ -7,22 +7,24 @@
         action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
         :beforeUpload='beforeUpload'
         @change='handleChange' >
-          <a-button class='fullSize flexItemAdp'>Upload picture</a-button>
+          <a-button class='fullSize flexItemAdp'>
+            {{$lang.messages.uploadPicture}}
+          </a-button>
       </a-upload>
       <img 
-        class='pointer fullSize margVert radius'
+        class='pointer fullSize margDown margUp radius'
         v-if='imageUrl' 
-        :src='imageUrl' 
+        :src='getRecogResult.mainImgRecog || imageUrl' 
       />
       <img 
-        class='pointer fullSize margVert radius'
+        class='pointer fullSize margDown margUp radius'
         v-else
         :src='defaultUrl' 
       />
       <a-button 
         v-if='imageUrl && !isRecognize' class='flexItemAdp fullSize' 
         @click='setRecognizeImage'>
-          Face recognition
+          {{$lang.messages.faceRec}}
       </a-button>
     </div>
     <!-- 2 Собс-но, сам результат -->
@@ -38,47 +40,52 @@
     reader.readAsDataURL(img);
   }
   import resultRecognitions from './ResultRecognitions';
+  import { mapMutations, mapActions, mapGetters } from 'vuex';
   export default {
     components : { resultRecognitions },
     data() {
       return {
-        loading: false,
         defaultUrl : require('../../content/images/backRecog.png'),
         imageUrl : '',
         isRecognize : false,
       };
     },
+    computed : {
+      ...mapGetters(['getRecogResult'])
+    },
     methods: {
+      ...mapActions(['getRecogData']),
+      ...mapMutations(['setCurrentMapValue']),
       handleChange( info ) {
         this.isRecognize = false;
         if ( info.file.status === 'uploading' ) {
-          this.loading = true;
+          this.setCurrentMapValue({ field : 'isLoading',  value : true });
           return;
         }
         if ( info.file.status === 'done' ) {
           // Get this url from response in real world.
           getBase64(info.file.originFileObj, imageUrl => {
+            this.setCurrentMapValue({ field : 'mainImgRecog',  value : null });
             this.imageUrl = imageUrl;
-            this.loading = false;
+            this.setCurrentMapValue({ field : 'isLoading',  value : false });
           });
         }
       },
       beforeUpload( file ) {
-        const isJPG = file.type === 'image/jpeg';
+        const isJPG = ( file.type === 'image/jpeg' ) || ( file.type === 'image/bmp' ) || ( file.type === 'image/png' );
         if ( !isJPG ) {
-          this.$message.error( 'You can only upload JPG file!' );
+          this.$message.error( this.$lang.messages.uploadErrorType );
         }
-        const isLt2M = file.size / 1024 / 1024 < 2;
+        const isLt2M = file.size / 1024 / 1024 < 20;
         if ( !isLt2M ) {
-          this.$message.error( 'Image must smaller than 2MB!' );
+          this.$message.error( this.$lang.messages.uploadErrorSize );
         }
         return isJPG && isLt2M;
       },
-      setRecognizeImage(){
-        setTimeout( ()=> {
-          this.isRecognize = true;
-          this.imageUrl = require('../../content/images/img_detected.jpg');
-        }, 1000 );
+      setRecognizeImage() {
+        this.setCurrentMapValue({ field : 'faceRecogResult',  value : [] });
+        this.isRecognize = true;
+        this.getRecogData( this.imageUrl );
       }
     },
   };
