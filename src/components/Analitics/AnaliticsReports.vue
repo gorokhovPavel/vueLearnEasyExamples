21d833538f766c5da37df48a1e3e7da804f32ec7
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-03-06 02:40:15 +0300

    settings

--- src/components/Analitics/AnaliticsReports.vue
+++ src/components/Analitics/AnaliticsReports.vue
@@ -1,40 +1,15 @@
 <template>
-    <div class='main'>
-		<table class='reports fullSize' border='0'>
-			<tr>
-				<td width='30%' v-for='(item, index) in reportDataList' :key='index' >
-					<h3>{{$lang.messages[item.titleLang]}}</h3>
-					<div @click="getPointReport(item.objForRepost)">
-						<a href="#">{{$lang.messages.today}}</a>
-					</div>
-					<div><a href="#">{{$lang.messages.for}} {{$lang.messages.last}} 24 {{$lang.messages.hours}}</a></div>
-					<div><a href="#">{{$lang.messages.for}} {{$lang.messages.last}} 7 {{$lang.messages.days}}</a></div>
-					<div><a href="#">{{$lang.messages.for}} {{$lang.messages.thisName}} {{$lang.messages.mounth}}</a></div>
-					<div><a href="#">{{$lang.messages.for}} {{$lang.messages.thisName}} {{$lang.messages.year}}</a></div>
-				</td>
-			</tr>
-			<tr v-if='false' >
-				<td>
-					<h2>by cameras</h2>
-					<div><a href="#">today</a></div>
-					<div><a href="#">last 24 hours</a></div>
-					<div><a href="#">last 7 days</a></div>
-					<div><a href="#">this month</a></div>
-					<div><a href="#">this year</a></div>
-				</td>
-				<td style="">
-					<h2>by floors</h2>
-					<div><a href="#">today</a></div>
-					<div><a href="#">last 24 hours</a></div>
-					<div><a href="#">last 7 days</a></div>
-					<div><a href="#">this month</a></div>
-					<div><a href="#">this year</a></div>
-				</td>
-				<td>
-					&nbsp;
-				</td>
-			</tr>
-		</table>
+    <div class='flexContainerAdp reportContainer fullSize'>
+		<div class='flexItemAdp reportPosAdp' v-for='(item, index) in reportDataList' :key='index' >
+			<h3 class='bold upper' >{{$lang.messages[item.titleLang]}}</h3>
+			<div @click="getPointReport(item.objForRepost)">
+				<a href="#">{{$lang.messages.today}}</a>
+			</div>
+			<div><a href="#">{{$lang.messages.for}} {{$lang.messages.last}} 24 {{$lang.messages.hours}}</a></div>
+			<div><a href="#">{{$lang.messages.for}} {{$lang.messages.last}} 7 {{$lang.messages.days}}</a></div>
+			<div><a href="#">{{$lang.messages.for}} {{$lang.messages.thisName}} {{$lang.messages.mounth}}</a></div>
+			<div><a href="#">{{$lang.messages.for}} {{$lang.messages.thisName}} {{$lang.messages.year}}</a></div>
+		</div>
 	</div>
 </template>
 <script >
@@ -70,11 +45,8 @@
 	}
 </script>
 <style lang="scss">
-	table.reports td {
-		text-align: 'center';
-	}
-	table.reports div {
+	//@import '../../content/style/adaptiveStyle.scss';
+	.reportContainer div {
 		line-height : 30px;
-		font-size : 13px;
 	}
 </style>
